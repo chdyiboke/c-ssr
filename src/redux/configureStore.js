12128c896d555f18e5474b8fbab847cbb462308b
caiyi04 <caiyi04@maoyan.com>
2021-02-20 19:19:46 +0800

    redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducers'
-
+// https://juejin.cn/post/6844903870246699022
 
 export default function configureStore(preloadedState) {
+  //通过createStore创建一个store，把reducer作为参数传入
   return createStore(
     rootReducer,
     preloadedState,
